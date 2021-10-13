b967f7862c923521a2b18fd7a493160162d84efc
Vasilii Lysenkov <lysenkov2@ya.ru>
2021-10-13 14:11:14 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String author;
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+
+    public Book() {
+        super();
+    }
+}
---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+import ru.netology.domain.Product;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+
+    public Smartphone() {
+        super();
+    }
+}
